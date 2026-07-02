3f07862aef6dc190a5ede0f0a02c0da57ae40ca1
Петренко Игорь Вадимович <igor333.petrenko@yandex.ru>
2026-07-02 12:01:17 +0300

    Revert "удалит лишние билды"
    This reverts commit 9a5cf3ff024ffb2ff5a038e357be4069510ef543.

---
+++ config.example.js
@@ -0,0 +1,5 @@
+// Скопируйте этот файл в config.js и вставьте реальную ссылку.
+// config.js добавлен в .gitignore и не должен попадать в репозиторий.
+window.LANDING_CONFIG = {
+  diskUrl: "https://disk.yandex.ru/i/PASTE-YOUR-LINK-HERE"
+};
